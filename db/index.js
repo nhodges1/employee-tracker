8088373a934711d759e18a3971cac3729c90c518
nhodges <hodges27@outlook.com>
2022-02-27 17:28:54 -0500

    updated indexjs

--- db/index.js
+++ db/index.js
@@ -12,4 +12,19 @@ class DB {
             "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
         );
     }
+
+    // find all employees except the given employee id
+    findAllPossibleManagers(employeeId) {
+        return this.connection.promise().query(
+            "SELECT id, first_name, last_name FROM employee WHERE id != ?",
+            employeeId
+        );
+    }
+
+    // create new employee
+    createEmployee(employee) {
+        return this.connection.promise().query("INSERT INTO employee SET ?", employee);
+    }
+
+    
 }
