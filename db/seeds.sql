491e7af77b2edbb038ff90de12ebbcc0eb3be8ed
nhodges <hodges27@outlook.com>
2022-02-27 14:30:28 -0500

    employee table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
+use employees;
+
 INSERT INTO department
     (name)
 VALUES
@@ -5,3 +7,15 @@ VALUES
     ('Engineering'),
     ('Finance'),
     ('Legal');
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
